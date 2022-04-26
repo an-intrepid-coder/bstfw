338239fe657f6e927fb656bd23ef52554651c461
Sam Gibson <sgibber2018@gmail.com>
2022-04-25 17:30:05 -0700

    the remaining two exercises from Ch 4

---
+++ interviews/interviews.R
@@ -0,0 +1,41 @@
+# Exercises 4 and 5 from Chapter 4:
+
+exactly <- function(k, n, p) {
+    return(choose(n, k) * (p^k) * ((1 - p)^(n-k)))
+}
+
+atLeast <- function(k, n, p) {
+   return(pbinom(k - 1, n, p, lower.tail=FALSE))
+}
+
+# The first scenario is asking what the
+# chance of getting >=2 job offers in a month
+# is, if the job seeker has a 1/5 chance of
+# getting an offer from each interview and
+# attends seven interviews.
+pAtLeastTwoOffersIn7 <- atLeast(2, 7, 1/5)
+
+# The second scenario ups the number of
+# interviews/month to 25, and lowers the
+# chance of getting an offer to 1/10.
+pAtLeastTwoOffersIn25 <- atLeast(2, 25, 1/10)
+
+# It asks the reader: Is the second scenario
+# more likely?
+secondScenarioMoreLikely <- pAtLeastTwoOffersIn25 > pAtLeastTwoOffersIn7
+
+data <- data.frame(
+    "experiment"=c(
+        "p(first scenario)",
+        "p(second scenario)",
+        "p(first) > p(second)"
+    ),
+    "result"=c(
+        pAtLeastTwoOffersIn7, 
+        pAtLeastTwoOffersIn25,
+        secondScenarioMoreLikely
+    )
+)
+
+print(data)
+
