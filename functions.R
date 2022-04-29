e17bace1745302ee14400ed973741e4fc33a7591
Sam Gibson <sgibber2018@gmail.com>
2022-04-29 02:50:57 -0700

    implemented pInRange() based on chapter 5

--- functions.R
+++ functions.R
@@ -12,9 +12,19 @@ exactly <- function(k, n, p) {
 
 # Returns sum(B(k; n, p) for all k in 1:n).
 # Or, the chance of at least k events happening
-# given the n trials and probability p of the event 
+# given n trials and probability p of the event 
 # occuring:
 atLeast <- function(k, n, p) {
    return(pbinom(k - 1, n, p, lower.tail=FALSE))
+}
+
+# This wraps integrate() and dbeta() to return the
+# probability that the true probability of an
+# event is within lo:hi based on an observed
+# number of successes and failures. It returns
+# the probability of a probability.
+pInRange <- function(s, f, lo, hi) {
+    fn <- function(p) dbeta(p, s, f)
+    return(integrate(fn, lo, hi)$value)
 }
 
